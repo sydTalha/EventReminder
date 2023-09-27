b9993e9ee0a75db9834b49cc610b055deb5c60ae
Syed Talha Rizwan <talharizwan013@gmail.com>
2023-09-27 11:12:57 +0500

    Initial Commit

---
+++ EventReminderTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventReminderTask
+//
+//  Created by Rizwan on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
